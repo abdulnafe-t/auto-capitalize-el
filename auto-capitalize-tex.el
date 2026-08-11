f7877847ced5d2788e948ef539c7e20642267ada
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-08-11 10:59:03 +0200

    [fix] Don’t check a-c-global-mode when blocking a-c-tex-mode
    That variable is global, and so if the user enables auto-capitalize-global-mode, it will
    always be t, even in buffers where we do not want to enable auto-capitalize-tex-mode (such
    as when the user calls auto-capitalize-tex-mode when AUCTeX is not available).

--- auto-capitalize-tex.el
+++ auto-capitalize-tex.el
@@ -143,7 +143,7 @@ will be enabled automatically."
       (auto-capitalize-tex-mode -1)
       (user-error "Auto-capitalize-tex-mode requires AUCTeX"))
 
-    (unless (or auto-capitalize-mode auto-capitalize-global-mode)
+    (unless auto-capitalize-mode
       (auto-capitalize-mode 1)
       (message "auto-capitalize-mode enabled for TeX support."))
 
